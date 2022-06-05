d574532962639599d272c2ac93574557d72147ca
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-06-05 10:25:48 +0900

    Delete Main.js

--- tmp/Main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Logger_1 = require("./Logger");
-function main() {
-    Logger_1.Logger.log("### START ###");
-    Logger_1.Logger.log("Hello World!");
-    Logger_1.Logger.log("--- END ---");
-}
-main();
