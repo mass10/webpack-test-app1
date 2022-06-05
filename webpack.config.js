c8e9a40c7763671ad3526a62eddc6ea07929475c
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-06-05 17:23:29 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,28 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './tmp/Main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|ts|tsx)$/,
-                exclude: /node_modules/,
-                // loader: 'ts-loader',
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx']
-    }
+	mode: 'development',
+	entry: './tmp/Main.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|ts|tsx)$/,
+				exclude: /node_modules/,
+				// loader: 'ts-loader',
+			}
+		]
+	},
+
+	// webpack にみつけさせるファイルの拡張子
+	resolve: {
+		extensions: [
+			'.js',
+			'.ts',
+			'.tsx'
+		]
+	}
 }
