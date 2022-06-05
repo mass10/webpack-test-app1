535995973ae5f6aebb4fd905e9e4b7f909187362
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-06-05 17:23:20 +0900

    Update main.rs

--- clis/src/main.rs
+++ clis/src/main.rs
@@ -1,32 +1,44 @@
 fn execute_command(command: &str, args: &[&str]) -> Result<(), Box<dyn std::error::Error>> {
-    let result = std::process::Command::new("cmd.exe")
-        .arg("/C")
-        .arg(command)
-        .args(args)
-        .spawn()?
-        .wait()?;
+	let result = std::process::Command::new("cmd.exe")
+		.arg("/C")
+		.arg(command)
+		.args(args)
+		.spawn()?
+		.wait()?;
 
-    if !result.success() {
-        let exit_code = result.code().unwrap_or(0);
-        println!("> {} {}", command, args.join(" "));
-        println!("FAILED ({})", exit_code);
-        std::process::exit(1);
-    }
+	if !result.success() {
+		let exit_code = result.code().unwrap_or(0);
+		println!("> {} {}", command, args.join(" "));
+		println!("FAILED ({})", exit_code);
+		std::process::exit(1);
+	}
 
-    return Ok(());
+	return Ok(());
+}
+
+fn delete_file(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+	if std::path::Path::new(path).exists() {
+		std::fs::remove_file(path)?;
+	}
+	return Ok(());
 }
 
 fn run() -> Result<(), Box<dyn std::error::Error>> {
-    execute_command("yarn", &[])?;
-    return Ok(());
+	delete_file("dist/bundle.js")?;
+
+	execute_command("yarn", &["install"])?;
+	execute_command("yarn", &["tsc", "--build", "tsconfig.json"])?;
+	execute_command("yarn", &["webpack", "--config", "webpack.config.js"])?;
+
+	return Ok(());
 }
 
 fn main() {
-    println!("### START ###");
-    let result = run();
-    if result.is_err() {
-        println!("[ERROR] {}", result.unwrap_err());
-        return;
-    }
-    println!("--- END ---");
+	println!("### START ###");
+	let result = run();
+	if result.is_err() {
+		println!("[ERROR] {}", result.unwrap_err());
+		return;
+	}
+	println!("--- END ---");
 }
